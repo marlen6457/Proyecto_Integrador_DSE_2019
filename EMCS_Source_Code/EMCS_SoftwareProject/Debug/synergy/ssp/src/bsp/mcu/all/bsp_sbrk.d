d92fa87958bc92bacf30676bc7327bbbfa8c7b0e
JorgeLopezCasillas <eng.jorgelopezcasillas@gmail.com>
2019-02-22 16:12:04 -0600

    Correction workspace
    CORRECT WAY
    D:\Documentos\Proyecto_Integrador_DSE_2019\EMCS_Source_Code\EMCS_SoftwareProject

---
+++ EMCS_Source_Code/EMCS_SoftwareProject/Debug/synergy/ssp/src/bsp/mcu/all/bsp_sbrk.d
@@ -0,0 +1,134 @@
+synergy/ssp/src/bsp/mcu/all/bsp_sbrk.o: \
+ ../synergy/ssp/src/bsp/mcu/all/bsp_sbrk.c \
+ D:\Documentos\Proyecto_Integrador_DSE_2019\EMCS_Source_Code\EMCS_SoftwareProject\synergy\ssp\inc\bsp/bsp_api.h \
+ d:\documentos\proyecto_integrador_dse_2019\emcs_source_code\emcs_softwareproject\synergy\ssp\inc\ssp_common_api.h \
+ d:\documentos\proyecto_integrador_dse_2019\emcs_source_code\emcs_softwareproject\synergy\ssp\inc\ssp_version.h \
+ D:\Documentos\Proyecto_Integrador_DSE_2019\EMCS_Source_Code\EMCS_SoftwareProject\synergy_cfg\ssp_cfg\bsp/bsp_cfg.h \
+ D:\Documentos\Proyecto_Integrador_DSE_2019\EMCS_Source_Code\EMCS_SoftwareProject\synergy_cfg\ssp_cfg\bsp/bsp_clock_cfg.h \
+ D:\Documentos\Proyecto_Integrador_DSE_2019\EMCS_Source_Code\EMCS_SoftwareProject\synergy_cfg\ssp_cfg\bsp/bsp_mcu_family_cfg.h \
+ D:\Documentos\Proyecto_Integrador_DSE_2019\EMCS_Source_Code\EMCS_SoftwareProject\synergy_cfg\ssp_cfg\bsp/bsp_mcu_device_pn_cfg.h \
+ D:\Documentos\Proyecto_Integrador_DSE_2019\EMCS_Source_Code\EMCS_SoftwareProject\synergy_cfg\ssp_cfg\bsp/bsp_mcu_device_cfg.h \
+ d:\documentos\proyecto_integrador_dse_2019\emcs_source_code\emcs_softwareproject\synergy\ssp\src\bsp\mcu\s7g2\bsp_mcu_info.h \
+ d:\documentos\proyecto_integrador_dse_2019\emcs_source_code\emcs_softwareproject\synergy\ssp\src\bsp\cmsis\device\renesas\s7g2\include\s7g2.h \
+ D:\Documentos\Proyecto_Integrador_DSE_2019\EMCS_Source_Code\EMCS_SoftwareProject\synergy\ssp\inc\bsp\cmsis\Include/core_cm4.h \
+ D:\Documentos\Proyecto_Integrador_DSE_2019\EMCS_Source_Code\EMCS_SoftwareProject\synergy\ssp\inc\bsp\cmsis\Include/cmsis_version.h \
+ D:\Documentos\Proyecto_Integrador_DSE_2019\EMCS_Source_Code\EMCS_SoftwareProject\synergy\ssp\inc\bsp\cmsis\Include/cmsis_compiler.h \
+ D:\Documentos\Proyecto_Integrador_DSE_2019\EMCS_Source_Code\EMCS_SoftwareProject\synergy\ssp\inc\bsp\cmsis\Include/cmsis_gcc.h \
+ D:\Documentos\Proyecto_Integrador_DSE_2019\EMCS_Source_Code\EMCS_SoftwareProject\synergy\ssp\inc\bsp\cmsis\Include/mpu_armv7.h \
+ d:\documentos\proyecto_integrador_dse_2019\emcs_source_code\emcs_softwareproject\synergy\ssp\src\bsp\cmsis\device\renesas\s7g2\include\system_S7G2.h \
+ d:\documentos\proyecto_integrador_dse_2019\emcs_source_code\emcs_softwareproject\synergy\ssp\src\bsp\mcu\s7g2\bsp_elc.h \
+ d:\documentos\proyecto_integrador_dse_2019\emcs_source_code\emcs_softwareproject\synergy\ssp\src\bsp\mcu\s7g2\bsp_cache.h \
+ D:\Documentos\Proyecto_Integrador_DSE_2019\EMCS_Source_Code\EMCS_SoftwareProject\synergy_cfg\ssp_cfg\bsp/bsp_board_cfg.h \
+ d:\documentos\proyecto_integrador_dse_2019\emcs_source_code\emcs_softwareproject\synergy\board\s7g2_sk\bsp.h \
+ d:\documentos\proyecto_integrador_dse_2019\emcs_source_code\emcs_softwareproject\synergy\board\s7g2_sk\bsp_init.h \
+ d:\documentos\proyecto_integrador_dse_2019\emcs_source_code\emcs_softwareproject\synergy\board\s7g2_sk\bsp_leds.h \
+ d:\documentos\proyecto_integrador_dse_2019\emcs_source_code\emcs_softwareproject\synergy\board\s7g2_sk\bsp_qspi.h \
+ d:\documentos\proyecto_integrador_dse_2019\emcs_source_code\emcs_softwareproject\synergy\board\s7g2_sk\bsp_ethernet_phy.h \
+ d:\documentos\proyecto_integrador_dse_2019\emcs_source_code\emcs_softwareproject\synergy\ssp\src\bsp\mcu\all\bsp_common.h \
+ d:\documentos\proyecto_integrador_dse_2019\emcs_source_code\emcs_softwareproject\synergy\ssp\inc\ssp_common_api.h \
+ d:\documentos\proyecto_integrador_dse_2019\emcs_source_code\emcs_softwareproject\synergy\ssp\src\bsp\mcu\all\bsp_compiler_support.h \
+ d:\documentos\proyecto_integrador_dse_2019\emcs_source_code\emcs_softwareproject\synergy\ssp\src\bsp\mcu\all\bsp_register_protection.h \
+ d:\documentos\proyecto_integrador_dse_2019\emcs_source_code\emcs_softwareproject\synergy\ssp\src\bsp\mcu\all\bsp_locking.h \
+ d:\documentos\proyecto_integrador_dse_2019\emcs_source_code\emcs_softwareproject\synergy\ssp\src\bsp\mcu\all\bsp_irq.h \
+ d:\documentos\proyecto_integrador_dse_2019\emcs_source_code\emcs_softwareproject\synergy\ssp\src\bsp\mcu\all\bsp_group_irq.h \
+ d:\documentos\proyecto_integrador_dse_2019\emcs_source_code\emcs_softwareproject\synergy\ssp\src\bsp\mcu\all\bsp_clocks.h \
+ D:\Documentos\Proyecto_Integrador_DSE_2019\EMCS_Source_Code\EMCS_SoftwareProject\synergy\ssp\inc\driver\api/r_ioport_api.h \
+ d:\documentos\proyecto_integrador_dse_2019\emcs_source_code\emcs_softwareproject\synergy\ssp\inc\ssp_features.h \
+ d:\documentos\proyecto_integrador_dse_2019\emcs_source_code\emcs_softwareproject\synergy\ssp\inc\ssp_common_api.h \
+ d:\documentos\proyecto_integrador_dse_2019\emcs_source_code\emcs_softwareproject\synergy\ssp\src\bsp\mcu\all\bsp_compiler_support.h \
+ D:\Documentos\Proyecto_Integrador_DSE_2019\EMCS_Source_Code\EMCS_SoftwareProject\synergy\ssp\inc\driver\instances/r_fmi.h \
+ D:\Documentos\Proyecto_Integrador_DSE_2019\EMCS_Source_Code\EMCS_SoftwareProject\synergy\ssp\inc\driver\api/r_fmi_api.h \
+ d:\documentos\proyecto_integrador_dse_2019\emcs_source_code\emcs_softwareproject\synergy\ssp\src\bsp\mcu\all\bsp_common_leds.h \
+ d:\documentos\proyecto_integrador_dse_2019\emcs_source_code\emcs_softwareproject\synergy\ssp\src\bsp\mcu\all\bsp_delay.h \
+ d:\documentos\proyecto_integrador_dse_2019\emcs_source_code\emcs_softwareproject\synergy\ssp\src\bsp\mcu\all\bsp_common.h \
+ d:\documentos\proyecto_integrador_dse_2019\emcs_source_code\emcs_softwareproject\synergy\ssp\src\bsp\mcu\all\bsp_feature.h \
+ d:\documentos\proyecto_integrador_dse_2019\emcs_source_code\emcs_softwareproject\synergy\ssp\src\bsp\mcu\all\bsp_mcu_api.h \
+ d:\documentos\proyecto_integrador_dse_2019\emcs_source_code\emcs_softwareproject\synergy\ssp\src\bsp\mcu\all\bsp_error_checking.h
+
+D:\Documentos\Proyecto_Integrador_DSE_2019\EMCS_Source_Code\EMCS_SoftwareProject\synergy\ssp\inc\bsp/bsp_api.h:
+
+d:\documentos\proyecto_integrador_dse_2019\emcs_source_code\emcs_softwareproject\synergy\ssp\inc\ssp_common_api.h:
+
+d:\documentos\proyecto_integrador_dse_2019\emcs_source_code\emcs_softwareproject\synergy\ssp\inc\ssp_version.h:
+
+D:\Documentos\Proyecto_Integrador_DSE_2019\EMCS_Source_Code\EMCS_SoftwareProject\synergy_cfg\ssp_cfg\bsp/bsp_cfg.h:
+
+D:\Documentos\Proyecto_Integrador_DSE_2019\EMCS_Source_Code\EMCS_SoftwareProject\synergy_cfg\ssp_cfg\bsp/bsp_clock_cfg.h:
+
+D:\Documentos\Proyecto_Integrador_DSE_2019\EMCS_Source_Code\EMCS_SoftwareProject\synergy_cfg\ssp_cfg\bsp/bsp_mcu_family_cfg.h:
+
+D:\Documentos\Proyecto_Integrador_DSE_2019\EMCS_Source_Code\EMCS_SoftwareProject\synergy_cfg\ssp_cfg\bsp/bsp_mcu_device_pn_cfg.h:
+
+D:\Documentos\Proyecto_Integrador_DSE_2019\EMCS_Source_Code\EMCS_SoftwareProject\synergy_cfg\ssp_cfg\bsp/bsp_mcu_device_cfg.h:
+
+d:\documentos\proyecto_integrador_dse_2019\emcs_source_code\emcs_softwareproject\synergy\ssp\src\bsp\mcu\s7g2\bsp_mcu_info.h:
+
+d:\documentos\proyecto_integrador_dse_2019\emcs_source_code\emcs_softwareproject\synergy\ssp\src\bsp\cmsis\device\renesas\s7g2\include\s7g2.h:
+
+D:\Documentos\Proyecto_Integrador_DSE_2019\EMCS_Source_Code\EMCS_SoftwareProject\synergy\ssp\inc\bsp\cmsis\Include/core_cm4.h:
+
+D:\Documentos\Proyecto_Integrador_DSE_2019\EMCS_Source_Code\EMCS_SoftwareProject\synergy\ssp\inc\bsp\cmsis\Include/cmsis_version.h:
+
+D:\Documentos\Proyecto_Integrador_DSE_2019\EMCS_Source_Code\EMCS_SoftwareProject\synergy\ssp\inc\bsp\cmsis\Include/cmsis_compiler.h:
+
+D:\Documentos\Proyecto_Integrador_DSE_2019\EMCS_Source_Code\EMCS_SoftwareProject\synergy\ssp\inc\bsp\cmsis\Include/cmsis_gcc.h:
+
+D:\Documentos\Proyecto_Integrador_DSE_2019\EMCS_Source_Code\EMCS_SoftwareProject\synergy\ssp\inc\bsp\cmsis\Include/mpu_armv7.h:
+
+d:\documentos\proyecto_integrador_dse_2019\emcs_source_code\emcs_softwareproject\synergy\ssp\src\bsp\cmsis\device\renesas\s7g2\include\system_S7G2.h:
+
+d:\documentos\proyecto_integrador_dse_2019\emcs_source_code\emcs_softwareproject\synergy\ssp\src\bsp\mcu\s7g2\bsp_elc.h:
+
+d:\documentos\proyecto_integrador_dse_2019\emcs_source_code\emcs_softwareproject\synergy\ssp\src\bsp\mcu\s7g2\bsp_cache.h:
+
+D:\Documentos\Proyecto_Integrador_DSE_2019\EMCS_Source_Code\EMCS_SoftwareProject\synergy_cfg\ssp_cfg\bsp/bsp_board_cfg.h:
+
+d:\documentos\proyecto_integrador_dse_2019\emcs_source_code\emcs_softwareproject\synergy\board\s7g2_sk\bsp.h:
+
+d:\documentos\proyecto_integrador_dse_2019\emcs_source_code\emcs_softwareproject\synergy\board\s7g2_sk\bsp_init.h:
+
+d:\documentos\proyecto_integrador_dse_2019\emcs_source_code\emcs_softwareproject\synergy\board\s7g2_sk\bsp_leds.h:
+
+d:\documentos\proyecto_integrador_dse_2019\emcs_source_code\emcs_softwareproject\synergy\board\s7g2_sk\bsp_qspi.h:
+
+d:\documentos\proyecto_integrador_dse_2019\emcs_source_code\emcs_softwareproject\synergy\board\s7g2_sk\bsp_ethernet_phy.h:
+
+d:\documentos\proyecto_integrador_dse_2019\emcs_source_code\emcs_softwareproject\synergy\ssp\src\bsp\mcu\all\bsp_common.h:
+
+d:\documentos\proyecto_integrador_dse_2019\emcs_source_code\emcs_softwareproject\synergy\ssp\inc\ssp_common_api.h:
+
+d:\documentos\proyecto_integrador_dse_2019\emcs_source_code\emcs_softwareproject\synergy\ssp\src\bsp\mcu\all\bsp_compiler_support.h:
+
+d:\documentos\proyecto_integrador_dse_2019\emcs_source_code\emcs_softwareproject\synergy\ssp\src\bsp\mcu\all\bsp_register_protection.h:
+
+d:\documentos\proyecto_integrador_dse_2019\emcs_source_code\emcs_softwareproject\synergy\ssp\src\bsp\mcu\all\bsp_locking.h:
+
+d:\documentos\proyecto_integrador_dse_2019\emcs_source_code\emcs_softwareproject\synergy\ssp\src\bsp\mcu\all\bsp_irq.h:
+
+d:\documentos\proyecto_integrador_dse_2019\emcs_source_code\emcs_softwareproject\synergy\ssp\src\bsp\mcu\all\bsp_group_irq.h:
+
+d:\documentos\proyecto_integrador_dse_2019\emcs_source_code\emcs_softwareproject\synergy\ssp\src\bsp\mcu\all\bsp_clocks.h:
+
+D:\Documentos\Proyecto_Integrador_DSE_2019\EMCS_Source_Code\EMCS_SoftwareProject\synergy\ssp\inc\driver\api/r_ioport_api.h:
+
+d:\documentos\proyecto_integrador_dse_2019\emcs_source_code\emcs_softwareproject\synergy\ssp\inc\ssp_features.h:
+
+d:\documentos\proyecto_integrador_dse_2019\emcs_source_code\emcs_softwareproject\synergy\ssp\inc\ssp_common_api.h:
+
+d:\documentos\proyecto_integrador_dse_2019\emcs_source_code\emcs_softwareproject\synergy\ssp\src\bsp\mcu\all\bsp_compiler_support.h:
+
+D:\Documentos\Proyecto_Integrador_DSE_2019\EMCS_Source_Code\EMCS_SoftwareProject\synergy\ssp\inc\driver\instances/r_fmi.h:
+
+D:\Documentos\Proyecto_Integrador_DSE_2019\EMCS_Source_Code\EMCS_SoftwareProject\synergy\ssp\inc\driver\api/r_fmi_api.h:
+
+d:\documentos\proyecto_integrador_dse_2019\emcs_source_code\emcs_softwareproject\synergy\ssp\src\bsp\mcu\all\bsp_common_leds.h:
+
+d:\documentos\proyecto_integrador_dse_2019\emcs_source_code\emcs_softwareproject\synergy\ssp\src\bsp\mcu\all\bsp_delay.h:
+
+d:\documentos\proyecto_integrador_dse_2019\emcs_source_code\emcs_softwareproject\synergy\ssp\src\bsp\mcu\all\bsp_common.h:
+
+d:\documentos\proyecto_integrador_dse_2019\emcs_source_code\emcs_softwareproject\synergy\ssp\src\bsp\mcu\all\bsp_feature.h:
+
+d:\documentos\proyecto_integrador_dse_2019\emcs_source_code\emcs_softwareproject\synergy\ssp\src\bsp\mcu\all\bsp_mcu_api.h:
+
+d:\documentos\proyecto_integrador_dse_2019\emcs_source_code\emcs_softwareproject\synergy\ssp\src\bsp\mcu\all\bsp_error_checking.h:
---
+++ EMCS_Source_Code/EMCS_SoftwareProject/Debug/synergy/ssp/src/framework/tes/dave2d/dave_memory.d
@@ -0,0 +1,80 @@
+synergy/ssp/src/framework/tes/dave2d/dave_memory.o: ../synergy/ssp/src/framework/tes/dave2d/dave_memory.c \
+D:\\Documentos\\Proyecto_Integrador_DSE_2019\\EMCS_Source_Code\\EMCS_SoftwareProject\\synergy\\ssp\\inc\\framework\\tes\\dave_driver.h \
+D:\\Documentos\\Proyecto_Integrador_DSE_2019\\EMCS_Source_Code\\EMCS_SoftwareProject\\synergy\\ssp\\inc\\framework\\tes\\dave_types.h \
+D:\\Documentos\\Proyecto_Integrador_DSE_2019\\EMCS_Source_Code\\EMCS_SoftwareProject\\synergy\\ssp\\inc\\framework\\tes\\dave_driver.h \
+D:\\Documentos\\Proyecto_Integrador_DSE_2019\\EMCS_Source_Code\\EMCS_SoftwareProject\\synergy\\ssp\\inc\\framework\\tes/dave_errorcodes.h \
+D:\\Documentos\\Proyecto_Integrador_DSE_2019\\EMCS_Source_Code\\EMCS_SoftwareProject\\synergy\\ssp\\inc\\framework\\tes\\dave_driver.h \
+D:\\Documentos\\Proyecto_Integrador_DSE_2019\\EMCS_Source_Code\\EMCS_SoftwareProject\\synergy\\ssp\\inc\\framework\\tes/dave_base.h \
+D:\\Documentos\\Proyecto_Integrador_DSE_2019\\EMCS_Source_Code\\EMCS_SoftwareProject\\synergy\\ssp\\inc\\framework\\tes\\dave_driver.h \
+D:\\Documentos\\Proyecto_Integrador_DSE_2019\\EMCS_Source_Code\\EMCS_SoftwareProject\\synergy\\ssp\\inc\\framework\\tes/dave_math.h \
+D:\\Documentos\\Proyecto_Integrador_DSE_2019\\EMCS_Source_Code\\EMCS_SoftwareProject\\synergy\\ssp\\inc\\framework\\tes\\dave_driver.h \
+../synergy/ssp/src/framework/tes/dave2d/dave_intern.h \
+../synergy/ssp/src/framework/tes/dave2d/dave_registermap.h \
+../synergy/ssp/src/framework/tes/dave2d/dave_intern.h \
+../synergy/ssp/src/framework/tes/dave2d/dave_base.h \
+../synergy/ssp/src/framework/tes/dave2d/dave_intern.h \
+../synergy/ssp/src/framework/tes/dave2d/dave_dlist.h \
+../synergy/ssp/src/framework/tes/dave2d/dave_intern.h \
+../synergy/ssp/src/framework/tes/dave2d/dave_rbuffer.h \
+../synergy/ssp/src/framework/tes/dave2d/dave_intern.h \
+../synergy/ssp/src/framework/tes/dave2d/dave_hardware.h \
+../synergy/ssp/src/framework/tes/dave2d/dave_intern.h \
+../synergy/ssp/src/framework/tes/dave2d/dave_context.h \
+../synergy/ssp/src/framework/tes/dave2d/dave_gradient.h \
+../synergy/ssp/src/framework/tes/dave2d/dave_context.h \
+../synergy/ssp/src/framework/tes/dave2d/dave_intern.h \
+../synergy/ssp/src/framework/tes/dave2d/dave_memory.h \
+../synergy/ssp/src/framework/tes/dave2d/dave_base.h
+
+D:\\Documentos\\Proyecto_Integrador_DSE_2019\\EMCS_Source_Code\\EMCS_SoftwareProject\\synergy\\ssp\\inc\\framework\\tes\\dave_driver.h:
+
+D:\\Documentos\\Proyecto_Integrador_DSE_2019\\EMCS_Source_Code\\EMCS_SoftwareProject\\synergy\\ssp\\inc\\framework\\tes\\dave_types.h:
+
+D:\\Documentos\\Proyecto_Integrador_DSE_2019\\EMCS_Source_Code\\EMCS_SoftwareProject\\synergy\\ssp\\inc\\framework\\tes\\dave_driver.h:
+
+D:\\Documentos\\Proyecto_Integrador_DSE_2019\\EMCS_Source_Code\\EMCS_SoftwareProject\\synergy\\ssp\\inc\\framework\\tes/dave_errorcodes.h:
+
+D:\\Documentos\\Proyecto_Integrador_DSE_2019\\EMCS_Source_Code\\EMCS_SoftwareProject\\synergy\\ssp\\inc\\framework\\tes\\dave_driver.h:
+
+D:\\Documentos\\Proyecto_Integrador_DSE_2019\\EMCS_Source_Code\\EMCS_SoftwareProject\\synergy\\ssp\\inc\\framework\\tes/dave_base.h:
+
+D:\\Documentos\\Proyecto_Integrador_DSE_2019\\EMCS_Source_Code\\EMCS_SoftwareProject\\synergy\\ssp\\inc\\framework\\tes\\dave_driver.h:
+
+D:\\Documentos\\Proyecto_Integrador_DSE_2019\\EMCS_Source_Code\\EMCS_SoftwareProject\\synergy\\ssp\\inc\\framework\\tes/dave_math.h:
+
+D:\\Documentos\\Proyecto_Integrador_DSE_2019\\EMCS_Source_Code\\EMCS_SoftwareProject\\synergy\\ssp\\inc\\framework\\tes\\dave_driver.h:
+
+../synergy/ssp/src/framework/tes/dave2d/dave_intern.h:
+
+../synergy/ssp/src/framework/tes/dave2d/dave_registermap.h:
+
+../synergy/ssp/src/framework/tes/dave2d/dave_intern.h:
+
+../synergy/ssp/src/framework/tes/dave2d/dave_base.h:
+
+../synergy/ssp/src/framework/tes/dave2d/dave_intern.h:
+
+../synergy/ssp/src/framework/tes/dave2d/dave_dlist.h:
+
+../synergy/ssp/src/framework/tes/dave2d/dave_intern.h:
+
+../synergy/ssp/src/framework/tes/dave2d/dave_rbuffer.h:
+
+../synergy/ssp/src/framework/tes/dave2d/dave_intern.h:
+
+../synergy/ssp/src/framework/tes/dave2d/dave_hardware.h:
+
+../synergy/ssp/src/framework/tes/dave2d/dave_intern.h:
+
+../synergy/ssp/src/framework/tes/dave2d/dave_context.h:
+
+../synergy/ssp/src/framework/tes/dave2d/dave_gradient.h:
+
+../synergy/ssp/src/framework/tes/dave2d/dave_context.h:
+
+../synergy/ssp/src/framework/tes/dave2d/dave_intern.h:
+
+../synergy/ssp/src/framework/tes/dave2d/dave_memory.h:
+
+../synergy/ssp/src/framework/tes/dave2d/dave_base.h:
+
