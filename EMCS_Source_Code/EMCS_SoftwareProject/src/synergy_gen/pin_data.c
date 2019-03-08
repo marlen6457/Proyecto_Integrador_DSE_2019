408d62175216b17b1f52a81030675b33082d6ccd
JorgeLopezCasillas <eng.jorgelopezcasillas@gmail.com>
2019-03-08 12:52:54 -0600

    Input Capture Done testing
    Ready input capture but the IDO is the previus version

--- EMCS_Source_Code/EMCS_SoftwareProject/src/synergy_gen/pin_data.c
+++ EMCS_Source_Code/EMCS_SoftwareProject/src/synergy_gen/pin_data.c
@@ -49,6 +49,18 @@ const ioport_pin_cfg_t g_bsp_pin_cfg_data[] = {
 		.pin_cfg = (IOPORT_CFG_ANALOG_ENABLE),
 	},
 	{
+		.pin = IOPORT_PORT_01_PIN_00,
+		.pin_cfg = (IOPORT_CFG_PERIPHERAL_PIN | IOPORT_PERIPHERAL_SCI0_2_4_6_8),
+	},
+	{
+		.pin = IOPORT_PORT_01_PIN_01,
+		.pin_cfg = (IOPORT_CFG_PERIPHERAL_PIN | IOPORT_PERIPHERAL_SCI0_2_4_6_8),
+	},
+	{
+		.pin = IOPORT_PORT_01_PIN_02,
+		.pin_cfg = (IOPORT_CFG_PERIPHERAL_PIN | IOPORT_PERIPHERAL_SCI0_2_4_6_8),
+	},
+	{
 		.pin = IOPORT_PORT_01_PIN_03,
 		.pin_cfg = (IOPORT_CFG_PERIPHERAL_PIN | IOPORT_PERIPHERAL_GPT1),
 	},
